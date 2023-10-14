6e8fbc11b2c3ee0d9454e59bbfdfdc9021f9b8ee
Nikita Lafinskiy <niklafinskiy@gmail.com>
2023-10-14 15:24:47 +0200

    deploy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+  app.use(cors({ origin: 'https://sereni-fy.com', credentials: true }));
   await app.listen(3000);
 }
 bootstrap();
