4b36ad39916f3fdb60b1d0b62a482d2787fa9881
Nikita Lafinskiy <niklafinskiy@gmail.com>
2023-10-24 16:25:44 +0200

    deploy

--- src/global.filter.ts
+++ src/global.filter.ts
@@ -6,7 +6,7 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 
-@Catch()
+@Catch(Error)
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
