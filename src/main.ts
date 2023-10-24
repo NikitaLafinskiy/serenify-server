5daa0ecae3d4b6969173ff04d19a88cc01af7fba
Nikita Lafinskiy <niklafinskiy@gmail.com>
2023-10-24 15:53:44 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
 import * as cookieParser from 'cookie-parser';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  const corsOptions: CorsOptions = {
+  app.enableCors({
     origin: 'https://sereni-fy.com',
     credentials: true,
-  };
+  });
 
-  app.enableCors(corsOptions);
   await app.listen(8080);
 }
 bootstrap();
