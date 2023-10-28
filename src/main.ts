05718010f1eaae9ff0ba65c67c31d6b195d6480a
Nikita Lafinskiy <niklafinskiy@gmail.com>
2023-10-28 14:38:35 +0200

    deploy

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(8080);
+  await app.listen(8080, 'serenify');
 }
 bootstrap();
