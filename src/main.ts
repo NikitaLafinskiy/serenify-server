174e4b7f5ff74e827303f765db5d3607e52b05d6
Nikita Lafinskiy <niklafinskiy@gmail.com>
2023-11-07 15:14:49 +0100

    deploy

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
 import * as cookieParser from 'cookie-parser';
+import { GlobalExceptionFilter } from 'global.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(8080, 'serenify');
 }
 bootstrap();
